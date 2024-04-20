27ba49bd7f5fd7cac39f2a8d247a069893a408b2
Olga <minaeva9@gmail.com>
2024-04-20 15:00:35 -0400

    form componenet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Header from "./Header";
+import Form from "./Form";
+import { useState } from "react";
 
 export default function App() {
+  const [habits, setHabits] = useState([]);
+
+  function handleAddHabits(habit) {
+    setHabits((habits) => [...habits, habit]);
+  }
   return (
     <div className="container">
       <Header />
+      <Form onAddHabit={handleAddHabits} />
     </div>
   );
 }
---
+++ src/Form.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Form({ onAddHabit }) {
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newHabit = { description, isHabit: false, id: Date.now() };
+    onAddHabit(newHabit);
+    setDescription("");
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Habit"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
