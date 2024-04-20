567dfd2154cdb8970c3c345f0e6999add65f4d52
Olga <minaeva9@gmail.com>
2024-04-20 17:18:09 -0400

    ternary count<21

--- src/Habit.jsx
+++ src/Habit.jsx
@@ -1,14 +1,17 @@
 export default function Habit({ habit, onDeleteHabit, onIncrementHabit }) {
   return (
-    <li>
-      <span>{habit.description} </span>
-      <span>
-        {habit.count} day{habit.count > 1 ? "s" : ""}
-      </span>
+    <>
+      {habit.count < 21 && (
+        <li>
+          <span>{habit.description} </span>
+          <span>
+            {habit.count} day{habit.count > 1 ? "s" : ""}
+          </span>
 
-      <button onClick={() => onIncrementHabit(habit.id)}>➕</button>
-      <button onClick={() => onDeleteHabit(habit.id)}>❌</button>
-    </li>
-    //  {habit.count >= 21 && <span>Done</span>}
+          <button onClick={() => onIncrementHabit(habit.id)}>➕</button>
+          <button onClick={() => onDeleteHabit(habit.id)}>❌</button>
+        </li>
+      )}
+    </>
   );
 }
